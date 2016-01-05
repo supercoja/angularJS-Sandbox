326252a3aaf7a6e8ed65302af450e1dc8683c90a
Thiago <taalmeid@yahoo.com.br>
2016-01-05 19:32:15 -0200

    ng-repeat - repos

--- angular-pluralSight/app.js
+++ angular-pluralSight/app.js
@@ -5,14 +5,29 @@
 
      var onUserComplete = function(response){
          $scope.user = response.data;
-     }
-     
-     $http.get("https://api.github.com/users/supercoja").then(onUserComplete);
-     
-     $scope.message = ' Angular JS';
-
+         $http.get($scope.user.repos_url)
+         .then(onRepos, onError)
      };
      
+     var onRepos = function(response)
+     {
+         $scope.repos = response.data;
+     };
+     
+     var onError = function(reason){
+         $scope.error = "Could Not Fetch Data";
+     };
+     
+     $scope.search = function(username)
+     {
+         $http.get("https://api.github.com/users/" + $scope.username).then(onUserComplete, onError);         
+     };
+     
+     $scope.username= "angular";
+     $scope.message = ' Angular JS';
+     };
+
      app.controller('MainController',["$scope","$http",MainController]);
+
 }());
 
